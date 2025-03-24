9f88a24b3cac75155e9d772d4718f770b1e320c8
VulpeenGX <alejandro.prg.trb@gmail.com>
2025-03-24 20:01:11 +0100

    navbar function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+    activeTab: string = ''; // Inicialmente no hay ninguna pestaña activa
+  
+    selectTab(tab: string): void {
+      this.activeTab = tab; // Establece la pestaña activa
+    }
+  
+  
 }
